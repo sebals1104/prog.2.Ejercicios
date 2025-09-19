809438036881231df4f85e213d9e697d4b0370fd
Sebastian Alsleben <sebasalsleben079@gmail.com>
2025-09-19 12:41:08 -0400

    commit

--- iniciacion.cpp
+++ iniciacion.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     cout << "¡Hola Mundo desde Git!" << endl;
     cout << "Mi nombre es: Sebastian Alsleben" << endl;
+    cout << "Mi correo es:" << endl;
     return 0;
 }
