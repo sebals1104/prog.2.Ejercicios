d09449a0585548fa84801cbe5bb2717e537b1315
Sebastian Alsleben <sebasalsleben079@gmail.com>
2025-09-19 13:10:05 -0400

    add nueva funcionalidad

---
+++ calculadora.cpp
@@ -0,0 +1,12 @@
+// calculadora.cpp
+#include <iostream>
+using namespace std;
+
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    cout << "5 + 3 = " << sumar(5, 3) << endl;
+    return 0;
+}
