d8359bae00faf4f1180bf8813261af007f371e36
Sebastian Alsleben <sebasalsleben079@gmail.com>
2025-09-19 12:37:08 -0400

    -feat: crea el programa hola mundo

---
+++ iniciacion.cpp
@@ -0,0 +1,9 @@
+// hola_mundo.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "¡Hola Mundo desde Git!" << endl;
+    cout << "Mi nombre es: Sebastian Alsleben" << endl;
+    return 0;
+}
