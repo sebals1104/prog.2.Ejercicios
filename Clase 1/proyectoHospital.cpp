7ce8743525a18e0dbaadc44132284819b714b401
Sebastian Alsleben <sebasalsleben079@gmail.com>
2025-10-06 18:43:26 -0400

    inicio proyecto hospital

---
+++ Clase 1/proyectoHospital.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <cstring>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+struct Hospital{
+    char nombre[100];
+    char direccion[200];
+    char telefono[15];
+
+    Doctor* doctores;
+    int cantidadDoctores;
+    int capacidadDoctores;
+    
+    Cita* citas;
+    int cantidadCitas;
+    int capacidadCitas;
+    
+    int siguienteIdPaciente;
+    int siguienteIdDoctor;
+    int siguienteIdCita;
+    int siguienteIdConsulta;
+};
+
+struct Paciente{
+    int id;
+    char nombre[50];
+    char apellido[50];
+    char cedula[20];
+    int edad;
+    char sexo;
+    char tipoSangre[5];
+    char telefono[15];
+    char direccion[100];
+    char email[50];
+
+    Doctor* doctores;
+    int cantidadDoctores;
+    int capacidadDoctores;
+    
+    Cita* citas;
+    int cantidadCitas;
+    int capacidadCitas;
+    
+    int siguienteIdPaciente;
+    int siguienteIdDoctor;
+    int siguienteIdCita;
+    int siguienteIdConsulta;
+
+    bool activo;
+};
+
+struct HistorialMedico {
+    int idConsulta;
+    char fecha[11];
+    char hora[6];
+    char diagnostico[200];
+    char tratamiento[200];
+    char medicamentos[150];
+    int idDoctor;
+    float costo;
+};
+
+struct Doctor {
+    int id;
+    char nombre[50];
+    char apellido[50];
+    char cedula[20];
+    char especialidad[50];
+    int aniosExperiencia;
+    float costoConsulta;
+    char horarioAtencion[50];
+    char telefono[15];
+    char email[50];
+
+    int* pacientesAsignados;
+    int cantidadPacientes;
+    int capacidadPacientes;
+    
+    int* citasAgendadas;
+    int cantidadCitas;
+    int capacidadCitas;
+};
+
+struct Cita {
+    int id;
+    int idPaciente;
+    int idDoctor;
+    char fecha[11];
+    char hora[6];
+    char motivo[150];
+    char estado[20];
+    char observaciones[200];
+    bool atendida;
+};
+
+void menu(){
+    cout << "==="  "===" << endl;
+    cout << "1. Crear Hospital" << endl;
+    cout << "2. Registro de pacientes y doctores" << endl;
+    cout << "3. Agendar citas" << endl;
+    cout << "4. Consultar citas" << endl;
+    cout << "5. Busqueda y filtros" << endl;
+    cout << "6. Eliminar registros" << endl;
+    cout << "7. Salir" << endl;
+    cout << "Seleccione una opción: ";
+}
